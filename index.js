577b174f82af404b9d904614a55635be17db94ff
jasim-uddin <jasimuddin2js@gmail.com>
2025-07-26 02:09:27 +0600

    mongodb database added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-// const jwt = require('jsonwebtoken');
-// const { MongoClient, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -10,13 +10,28 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// const uri = process.env.MONGO_URI;
-// const client = new MongoClient(uri);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jkw3zok.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const client = new MongoClient(uri);
 
-app.get('/', (req, res) => {
-  res.send('Edu Platform Backend is Running');
-});
+async function run() {
+  try {
+    await client.connect();
+    const db = client.db('Edu-Manage-System');
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+    const classesCollection = db.collection('classes');
+
+    app.get('/', (req, res) => {
+      res.send('Edu Platform Backend is Running');
+    });
+
+    // TODO: add auth, classes, user routes
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run();
